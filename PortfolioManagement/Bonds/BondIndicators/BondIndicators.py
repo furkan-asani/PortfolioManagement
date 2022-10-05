e4f61ec43fad384a494e2866422cd83ea73827d1
furkan-asani <asfu1011@h-ka.de>
2022-10-05 18:03:32 +0000

    refactored and integrated the new search assets api from investiny

--- PortfolioManagement/Bonds/BondIndicators/BondIndicators.py
+++ PortfolioManagement/Bonds/BondIndicators/BondIndicators.py
@@ -1,7 +1,103 @@
 class BondIndicators:
 
     def __init__(self, sqlConnection):
-        self.sqlConnection = sqlConnection
+        self.__sqlConnection = sqlConnection
 
-    def getDepotDataframe():
+    def getDepotDataframe(self):
         pass
+
+    def calculateProfitOrLossForASale(self, transactionId: int):
+        # Get all buys until the date of the transaction
+        # Get all sales without the referenced sale until the date of the transaction
+        # Get the referenced transaction sales data
+
+        getReferencedTransactionDataSQLStatement = f'Select * from transaction where "transactionID" = {transactionId}'
+
+        transactionResult = self.__sqlConnection.execute(getReferencedTransactionDataSQLStatement)
+        fetchedTransaction = transactionResult.fetchall()[0]
+        
+        transactionDate = fetchedTransaction[3]
+        isin = fetchedTransaction[0]
+
+        saleToCalculate = Sale(fetchedTransaction[2], fetchedTransaction[6]) 
+
+        allPurchasesList =  self.__getAllPurchases(transactionDate, isin)
+
+        allSalesList = self.__getAllSalesList(transactionDate, isin)
+
+        for sale in allSalesList:
+            for purchase in allPurchasesList:
+                if(purchase.amount == 0):
+                    continue
+                purchaseAmountCopy = purchase.amount
+                purchase.amount = max(purchase.amount - sale.amount, 0)
+                sale.amount = max(sale.amount - purchaseAmountCopy, 0)
+                if(purchase.amount == 0):
+                    continue
+                if(sale.amount == 0):
+                    break
+        
+        allPurchasesListIterator = filter(lambda purchase: (purchase.amount > 0), allPurchasesList)
+
+        allPurchasesList = list(allPurchasesListIterator)
+
+        totalProfit = 0
+
+        for purchase in allPurchasesList:
+            totalProfit += min(saleToCalculate.amount, purchase.amount) * (saleToCalculate.price - purchase.price)
+            saleToCalculateAmountCopy = saleToCalculate.amount
+            saleToCalculate.amount = max(saleToCalculate.amount - purchase.amount, 0)
+            purchase.amount = max(purchase.amount - saleToCalculateAmountCopy, 0)
+            if(saleToCalculate.amount == 0):
+                break
+
+        return totalProfit
+
+    def __getAllSalesList(self, transactionDate, isin):
+        getAllSalesSqlStatement = f"Select * from transaction where isin LIKE '{isin}' AND typeoftransaction LIKE 'sell' AND transactiondate < '{transactionDate}'"
+
+        allSales = self.__sqlConnection.execute(getAllSalesSqlStatement)
+
+        fetchedAllSales = allSales.fetchall()
+
+        allSalesList = []
+
+        for sale in fetchedAllSales:
+            saleObject = Sale(sale[2], sale[6])
+            allSalesList.append(saleObject)
+
+        return allSalesList
+
+    def __getAllPurchases(self, transactionDate, isin):
+        getAllPurchasesSqlStatement = f"Select * from transaction where isin LIKE '{isin}' AND typeoftransaction LIKE 'buy' AND transactiondate <= '{transactionDate}'"
+
+        allPurchases = self.__sqlConnection.execute(getAllPurchasesSqlStatement)
+
+        fetchedAllPurchases = allPurchases.fetchall()
+
+        allPurchasesList = []
+        for purchase in fetchedAllPurchases:
+            purchaseObject = Purchase(purchase[2], purchase[6])
+            allPurchasesList.append(purchaseObject)
+
+        return allPurchasesList
+
+class Transaction:
+    def __init__(self, amount, price):
+        self.amount = amount
+        self.price = price
+
+class Sale(Transaction):
+    pass
+
+class Purchase(Transaction):
+    pass
+
+import sqlalchemy
+connectionString = "postgresql+psycopg2://root:password@postgres_db:5432/portfolio"
+engine = sqlalchemy.create_engine(connectionString)
+connection = engine.connect()
+
+bondIndicators = BondIndicators(connection)
+
+bondIndicators.calculateProfitOrLossForASale(62)
