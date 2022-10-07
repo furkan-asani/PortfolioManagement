294ace15c41d7aebae2446cbbd3b2989d9f32ff5
furkan-asani <asfu1011@h-ka.de>
2022-10-07 06:36:54 +0000

    Implemented the transaction and transaction helper classes in order to insert transactions into the database

---
+++ Bonds/BondIndicators/Transactions.py
@@ -0,0 +1,48 @@
+from datetime import date
+class Transaction:
+    def __init__(
+        self,
+        isin: str,
+        wkn: str,
+        amountofbonds: float,
+        transactiondate: date,
+        typeoftransaction: str,
+        transactioncomment: str,
+        price: float,
+        depot: str,
+        currency: str,
+        exchange: str,
+    ):
+        self.isin = isin
+        self.wkn = wkn
+        self.amountofbonds = amountofbonds
+        self.transactiondate = transactiondate
+        self.typeoftransaction = typeoftransaction
+        self.transactioncomment = transactioncomment
+        self.price = price
+        self.depot = depot
+        self.currency = currency
+        self.exchange = exchange
+
+class TransactionHelper:
+    def __init__(self, sqlConnection):
+        self.__sqlConnection = sqlConnection
+        
+    def insertIntoDatabase(self, transaction: Transaction):
+        insertSqlStatement =f"""
+            INSERT INTO transaction (isin, wkn, amountofbonds, transactiondate, typeoftransaction, transactioncomment, price, depot, currency, exchange)
+            VALUES ('{transaction.isin}', '{transaction.wkn}', {transaction.amountofbonds}, '{transaction.transactiondate}', '{transaction.typeoftransaction}', '{transaction.transactioncomment}', {transaction.price}, '{transaction.depot}', '{transaction.currency}', '{transaction.exchange}')
+        """
+        self.__sqlConnection.execute(insertSqlStatement)
+
+
+import sqlalchemy
+connectionString = "postgresql+psycopg2://root:password@postgres_db:5432/portfolio"
+engine = sqlalchemy.create_engine(connectionString)
+connection = engine.connect()
+
+example = TransactionHelper(connection)
+
+transaction = Transaction('12345678','12345678', 10, date(year=2022, month=10, day=7), 'buy', 'Sample', 32.58, 'DKB', 'EUR', 'Xetra')
+
+example.insertIntoDatabase(transaction=transaction)
