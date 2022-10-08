2589f81e4a739d060471f9fc54b50bbfdcbd4f96
furkan-asani <asfu1011@h-ka.de>
2022-10-08 09:40:16 +0000

    Initial implementation of the associate PDFs logic without tests

--- Bonds/BondIndicators/AssociatePDFs.py
+++ Bonds/BondIndicators/AssociatePDFs.py
@@ -1,38 +1,94 @@
+from datetime import date
 import os
+
+
+class PDFEntry:
+    def __init__(self, date: date, type: str, comment: str, filepath: str):
+        self.date = date
+        self.type = type
+        self.comment = comment
+        self.filepath = filepath
+
 
 class AssociatePDFs:
     def __init__(self, sqlConnection):
         self.__sqlConnection = sqlConnection
-        
+        self.__fileDropPath = "/home/PortfolioManagement/FileDrop"
+        self.__onlyFiles = onlyFiles = [
+            f
+            for f in os.listdir(self.__fileDropPath)
+            if os.path.isfile(os.path.join(self.__fileDropPath, f))
+        ]
+        self.__basepath = "/home/PortfolioManagement/PDFs/Transactions"
 
-    def associatePDFsWithTransaction(self, transactionId: int):
-        getTransactionStatement = f'SELECT * from transaction WHERE "transactionID" = {transactionId}'
-        transactionResult = self.__sqlConnection.execute(getTransactionStatement)
-        fetchedTransactionResult = transactionResult.fetchall()[0]
+    def associatePDFsWithTransaction(
+        self, transactionId: int, typeOfPDFs: str = "Transaction", comment: str = ""
+    ):
+        # TODO Falls es keine BondId rufe eine Funktion auf, welche die BondIds extrahiert und in die DB einpflegt
+        fetchedTransactionResult = self.__getTransaction(transactionId)
         date = fetchedTransactionResult[3]
+
+        fetchedBondId = self.__getBondId(fetchedTransactionResult)
+
+        destinationPath = f"{self.__basepath}/{date.year}/{date.month}/{date.day}"
+        self.__createFolderIfDoesntExist(destinationPath)
+
+        self.__iterateOverFileDropAndCreateAssociations(
+            transactionId, typeOfPDFs, comment, date, fetchedBondId, destinationPath
+        )
+
+    def __iterateOverFileDropAndCreateAssociations(
+        self, transactionId, typeOfPDFs, comment, date, fetchedBondId, destinationPath
+    ):
         id = 1
+        for file in self.__onlyFiles:
+            newPath = f"{destinationPath}/TransactionPDF_{id}.pdf"
+            os.rename(
+                os.path.join(self.__fileDropPath, file),
+                newPath,
+            )
+            pdfEntry = PDFEntry(date, typeOfPDFs, comment, newPath)
+            pdfId = self.__insertPDFIntoDatabase(pdfEntry)
+            self.__insertAssociationIntoDatabase(transactionId, fetchedBondId, pdfId)
+            id += 1
+
+    def __insertAssociationIntoDatabase(self, transactionId, fetchedBondId, pdfId):
+        insertAssociationSqlStatement = f'INSERT INTO "AssociatedFiles" ( "FKBondId", fkpdfid, "FKTransactionID") VALUES ({fetchedBondId}, {pdfId}, {transactionId})'
+        self.__sqlConnection.execute(insertAssociationSqlStatement)
+
+    def __createFolderIfDoesntExist(self, destinationPath):
+        if not os.path.exists(destinationPath):
+            os.makedirs(destinationPath)
+
+    def __getBondId(self, fetchedTransactionResult):
         getBondIdSqlStatement = f'SELECT "BondID" FROM "Bond" WHERE isin = \'{fetchedTransactionResult[0]}\''
         bondIdResult = self.__sqlConnection.execute(getBondIdSqlStatement)
         fetchedBondId = bondIdResult.fetchall()[0][0]
-        
-        insertAssociationSqlStatement = f'INSERT INTO "AssociatedFiles" ( "FKBondId", fkpdfid, "FKTransactionID") VALUES ({fetchedBondId}, , {transactionId})'
-        # use the date of the transaction in order to determine the folder in which the pdfs should be moved
-        fileDropPath = "/home/PortfolioManagement/FileDrop"
-        basePath = "/home/PortfolioManagement/PDFs/Transactions"
-        destinationPath = f"{basePath}/{date.year}/{date.month}/{date.day}"
-        if not os.path.exists(destinationPath):
-            os.makedirs(destinationPath)
-        onlyFiles = [f for f in os.listdir(fileDropPath) if os.path.isfile(os.path.join(fileDropPath, f))]
-        for file in onlyFiles:
-            os.rename(os.path.join(fileDropPath, file), f'{destinationPath}/TransactionPDF{id}.pdf')
-            self.__sqlConnection.execute()
-            id += 1
-        pass
+        return fetchedBondId
 
-    def insertPdfsIntoDatabase(self):
-        pass
+    def __getTransaction(self, transactionId):
+        getTransactionStatement = (
+            f'SELECT * from transaction WHERE "transactionID" = {transactionId}'
+        )
+        transactionResult = self.__sqlConnection.execute(getTransactionStatement)
+        fetchedTransactionResult = transactionResult.fetchall()[0]
+        return fetchedTransactionResult
+
+    def __insertPDFIntoDatabase(self, pdfEntry: PDFEntry) -> int:
+
+        insertPDFIntoDatabaseSqlStatement = f"INSERT INTO \"PDFs\" (date, type, comment, filepath) VALUES ('{pdfEntry.date}', '{pdfEntry.type}', '{pdfEntry.comment}', '{pdfEntry.filepath}')"
+        self.__sqlConnection.execute(insertPDFIntoDatabaseSqlStatement)
+
+        getLatestPDFIdSqlStatement = (
+            'SELECT pdfid FROM "PDFs" ORDER By pdfid DESC LIMIT 1'
+        )
+        latestPDFIdResult = self.__sqlConnection.execute(getLatestPDFIdSqlStatement)
+        fetchedLatestPDFId = latestPDFIdResult.fetchall()
+        return fetchedLatestPDFId[0][0]
+
 
 import sqlalchemy
+
 connectionString = "postgresql+psycopg2://root:password@postgres_db:5432/portfolio"
 engine = sqlalchemy.create_engine(connectionString)
 connection = engine.connect()
