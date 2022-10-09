e278d0aadf82795491794f68d112b98da7027ae6
furkan-asani <asfu1011@h-ka.de>
2022-10-09 09:52:51 +0000

    Implemented the import for the consors depot

--- Bonds/BondIndicators/ImportScripts.py
+++ Bonds/BondIndicators/ImportScripts.py
@@ -4,7 +4,6 @@ import pandas as pd
 import Transactions
 
 class ImportOnVista:
-    
     def __init__(self, sqlConnection, transactionHelper: Transactions.TransactionHelper):
         # You may have to adjust this path according to your path and filename
         self.__filePath = '/home/PortfolioManagement/Import/OnVista.csv'
@@ -81,6 +80,36 @@ class ImportDKB:
 
         self.__sqlConnection.execute(insertBondSQLStatement)
 
+class ImportConsors:
+    def __init__(self, sqlConnection, transactionHelper: Transactions.TransactionHelper):
+        self.__sqlConnection = sqlConnection
+        self.__transactionHelper = transactionHelper
+        self.__filePath = '/home/PortfolioManagement/Import/Consors.xlsx'
+        
+    def readPositionsAndStoreInDatabase(self):
+        df = pd.read_excel(io=self.__filePath, header=6)
+        for row in df.itertuples():
+            isInvalidRow = row[3] is NaN
+            if isInvalidRow:
+                continue
+
+            transaction = self.__insertIntoTransactionTable(row)
+
+            self.__insertIntoBondsTable(row, transaction)
+            
+    def __insertIntoTransactionTable(self, row):
+        transaction = Transactions.Transaction('', row[2], row[4], row[12], 'buy', '', row[5], 'Consors', row[6], row[13])
+
+        self.__transactionHelper.insertIntoDatabase(transaction)
+
+        return transaction
+
+    def __insertIntoBondsTable(self, row, transaction: Transactions.Transaction):
+        insertBondSQLStatement = f'INSERT INTO "Bond" (isin, wkn, "Comment", "Name") VALUES (\'{transaction.isin}\', \'{transaction.wkn}\', \'\', \'{row[1]}\')'
+
+        self.__sqlConnection.execute(insertBondSQLStatement)
+
+
 import sqlalchemy
 
 connectionString = "postgresql+psycopg2://root:password@postgres_db:5432/portfolio"
@@ -89,6 +118,6 @@ connection = engine.connect()
 
 transactionHelper = Transactions.TransactionHelper(connection)
 
-example =ImportDKB(connection, transactionHelper)
+example =ImportConsors(connection, transactionHelper)
 
 example.readPositionsAndStoreInDatabase()
