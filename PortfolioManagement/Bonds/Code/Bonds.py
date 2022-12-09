e23970250ff00803fb823d43b384117254b80970
furkan-asani <asfu1011@h-ka.de>
2022-12-09 11:49:32 +0000

    push latest changes

--- PortfolioManagement/Bonds/Code/Bonds.py
+++ PortfolioManagement/Bonds/Code/Bonds.py
@@ -3,17 +3,21 @@ import investiny.info
 
 from PriceDataInvestiny import PriceDataInvestiny
 
+
 class Bonds:
-    def __init__(self, sqlConnection, priceService):
+    def __init__(self, sqlConnection, priceService, isinToSymbolConverter):
         self.__sqlConnection = sqlConnection
         self.priceService = priceService
+        self.__isinToSymbolConverter = isinToSymbolConverter
         self.identifyNewBonds()
 
     def identifyNewBonds(self):
-        
+
         identifyNewBondsInTransactionSqlStatement = 'SELECT DISTINCT ISIN FROM transaction WHERE isin NOT IN (SELECT DISTINCT ISIN FROM "Bond")'
 
-        newBondsResult = self.__sqlConnection.execute(identifyNewBondsInTransactionSqlStatement)
+        newBondsResult = self.__sqlConnection.execute(
+            identifyNewBondsInTransactionSqlStatement
+        )
         fetchedNewBonds = newBondsResult.fetchall()
         mappedNewBonds = list(map(lambda bond: bond[0], fetchedNewBonds))
         self.mappedNewBonds = mappedNewBonds
@@ -25,11 +29,19 @@ class Bonds:
             name = bondInformation[0]["name"]
         pass
 
+    def storeSymbolForBond(self, isin: str):
+        symbol = self.__isinToSymbolConverter.getTickerForISIN(isin)
+        if symbol != "n/a":
+            self.__sqlConnection.execute(
+                f"UPDATE \"Bond\" SET SYMBOL = '{symbol}' WHERE isin LIKE '{isin}'"
+            )
+
+
 connectionString = "postgresql+psycopg2://root:password@postgres_db:5432/portfolio"
 engine = sqlalchemy.create_engine(connectionString)
 connection = engine.connect()
 priceService = PriceDataInvestiny(connection)
-bonds = Bonds(connection, priceService)
+# bonds = Bonds(connection, priceService)
 
-bonds.getNameByIsin()
-print(bonds.identifyNewBonds())
+# bonds.getNameByIsin()
+# print(bonds.identifyNewBonds())
